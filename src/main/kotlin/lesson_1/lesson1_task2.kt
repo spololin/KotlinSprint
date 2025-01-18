5feff926475387d2c6208bea6fe7afcd467c8dfd
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-18 17:16:01 +0300

    ks-1-2 create three variables and print

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+    val orderCount: Int = 75;
+    val thankText: String = "Спасибо за покупку!";
+
+    println(orderCount)
+    println(thankText)
+
+    var employeeCount: Int = 2000
+
+//    println(employeeCount)
+
+    employeeCount = 1999
+
+    println(employeeCount)
+}
+
