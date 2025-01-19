71fdfaa92735443b9340efb6373a11fb08b1dd2b
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-19 18:37:42 +0300

    ks-2-5 add two constants

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,11 +4,13 @@ import kotlin.math.*
 
 const val PERCENT_DEPOSIT = 16.7
 const val COUNT_ACCRUALS_IN_YEAR = 1
+const val TOTAL_PERCENTS = 100
+const val FIXED_VALUE = 1
 
 fun main() {
     val deposit = 70_000
     val year = 20
-    val finalDeposit = deposit * ((1 + (PERCENT_DEPOSIT / 100) / COUNT_ACCRUALS_IN_YEAR)).pow(COUNT_ACCRUALS_IN_YEAR * year)
+    val finalDeposit = deposit * ((FIXED_VALUE + (PERCENT_DEPOSIT / TOTAL_PERCENTS) / COUNT_ACCRUALS_IN_YEAR)).pow(COUNT_ACCRUALS_IN_YEAR * year)
 
     println(String.format("%.3f", finalDeposit))
 }
