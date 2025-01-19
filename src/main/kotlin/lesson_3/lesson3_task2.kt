0ca412a2e2a4f301e7485f06b9ae8f5eb43ccc7c
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-19 13:17:04 +0300

    ks-3-2 print username before and after wedding

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+    val userName: String = "Татьяна"
+    val userSurname: String = "Андреева"
+    val userPatronymic: String = "Сергеевна"
+    val userSurnameAfterWedding: String = "Сидорова"
+    val ageBeforeWedding: Int = 20
+    val ageAfterWedding: Int = 22
+
+    println("$userSurname $userName $userPatronymic: $ageBeforeWedding")
+    println("$userSurnameAfterWedding $userName $userPatronymic: $ageAfterWedding")
+
+}
