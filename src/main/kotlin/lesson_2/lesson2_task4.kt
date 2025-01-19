2afdb7afd79177db31e5bfdd96c0eaf00bee268c
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-19 18:29:13 +0300

    ks-2-4 add two constants

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,13 @@
 package lesson_2
 
 const val BUFF = 20
+const val TOTAL_PERCENTS = 100
 
 fun main() {
     val cristalOre = 7
     val ironOre = 11
 
-    println("CristalOre: ${cristalOre * (BUFF.toFloat() / 100)}")
-    println("ironOre: ${ironOre * (BUFF.toFloat() / 100)}")
+    println("CristalOre: ${cristalOre * (BUFF.toFloat() / TOTAL_PERCENTS)}")
+    println("ironOre: ${ironOre * (BUFF.toFloat() / TOTAL_PERCENTS)}")
 }
 
