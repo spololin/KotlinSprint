0a79ce95db4363ccf4ed3bebd30483ad512233e7
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-20 14:42:26 +0300

    ks-3-2 removed unnecessary variable

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,14 +1,15 @@
 package lesson_3
 
 fun main() {
-    val userName: String = "Татьяна"
-    val userSurname: String = "Андреева"
-    val userPatronymic: String = "Сергеевна"
-    val userSurnameAfterWedding: String = "Сидорова"
-    val ageBeforeWedding: Int = 20
-    val ageAfterWedding: Int = 22
+    val userName = "Татьяна"
+    var userSurname = "Андреева"
+    val userPatronymic = "Сергеевна"
+    var userAge = 20
 
-    println("$userSurname $userName $userPatronymic: $ageBeforeWedding")
-    println("$userSurnameAfterWedding $userName $userPatronymic: $ageAfterWedding")
+    println("$userSurname $userName $userPatronymic: $userAge")
 
+    userAge = 22
+    userSurname = "Сидорова"
+
+    println("$userSurname $userName $userPatronymic: $userAge")
 }
