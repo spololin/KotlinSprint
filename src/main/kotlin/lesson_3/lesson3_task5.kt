939628cbef8539804f354afaeb142547d0320315
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-20 14:34:06 +0300

    ks-3-5 replace split for vararg

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,15 +2,10 @@ package org.example.lesson_3
 
 fun main() {
     val stepData = "D2-D4;0"
-    val partsStepData = stepData.split(";")
-    val steps = partsStepData[0].split("-")
+    val partsStep = stepData.split(";", "-")
 
-    val stepStart = steps[0]
-    val stepEnd = steps[1]
-    val stepNumber = partsStepData[1]
-
-    println(stepStart)
-    println(stepEnd)
-    println(stepNumber)
+    println(partsStep[0])
+    println(partsStep[1])
+    println(partsStep[2])
 }
 
