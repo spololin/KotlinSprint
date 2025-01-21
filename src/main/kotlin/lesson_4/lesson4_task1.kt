c95eb7dc1646659f52b13d245433fd6ed63053fc
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-21 16:43:59 +0300

    ks-4-1 add line break in println

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -6,5 +6,8 @@ fun main() {
     val tableOccupancyToday = 13
     val tableOccupancyTomorrow = 9
 
-    println("Доступность столиков на сегодня: ${tableOccupancyToday < COUNT_TABLES}\nДоступность столиков на завтра: ${tableOccupancyTomorrow < COUNT_TABLES}")
+    println(
+        "Доступность столиков на сегодня: ${tableOccupancyToday < COUNT_TABLES}\n" +
+                "Доступность столиков на завтра: ${tableOccupancyTomorrow < COUNT_TABLES}"
+    )
 }
