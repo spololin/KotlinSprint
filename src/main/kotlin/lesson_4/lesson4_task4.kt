e4d8e4a46e2777f4eca11730bf5d60df6de9e76a
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-21 14:26:14 +0300

    ks-4-4 optimize algorithm

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,16 +1,15 @@
 package org.example.lesson_4
 
 fun main() {
-    val armExercises = 1
-    val legExercises = 0
-    val backExercises = 0
-    val absExercises = 1
     val dayExercises = 5
+    val isEven = dayExercises % 2 == 0
 
-    println("""
-        Упражнения для рук: ${dayExercises % 2 == armExercises}
-        Упражнения для ног: ${dayExercises % 2 == legExercises}
-        Упражнения для спины: ${dayExercises % 2 == backExercises}
-        Упражнения для пресса: ${dayExercises % 2 == absExercises}
-    """.trimIndent())
+    println(
+        """
+        Упражнения для рук: ${!isEven}
+        Упражнения для ног: $isEven
+        Упражнения для спины: $isEven
+        Упражнения для пресса: ${!isEven}
+    """.trimIndent()
+    )
 }
