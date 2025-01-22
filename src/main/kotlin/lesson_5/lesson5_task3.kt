c5400fbe28bc2e148e4d2390c9082d08114d79f0
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-22 15:31:25 +0300

    ks-5-3 change solution from arrays on conditions

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -4,16 +4,17 @@ const val RIDDLE_NUMBER_1 = 30
 const val RIDDLE_NUMBER_2 = 40
 
 fun main() {
-    val riddleNumbers = arrayOf(RIDDLE_NUMBER_1, RIDDLE_NUMBER_2).sorted()
-
     println("Please inter two number in range 0 to 42")
     val consoleNumber1 = readln().toInt()
     val consoleNumber2 = readln().toInt()
-    val consoleNumbers = arrayOf(consoleNumber1, consoleNumber2).sorted()
 
-    if (riddleNumbers.containsAll(consoleNumbers))
+    if ((consoleNumber1 == RIDDLE_NUMBER_1 && consoleNumber2 == RIDDLE_NUMBER_2) ||
+        (consoleNumber1 == RIDDLE_NUMBER_2 && consoleNumber2 == RIDDLE_NUMBER_1)
+    )
         println("Поздравляем! Вы выиграли главный приз!")
-    else if (riddleNumbers.indexOf(consoleNumbers[0]) != -1 || riddleNumbers.indexOf(consoleNumbers[1]) != -1)
+    else if ((consoleNumber1 == RIDDLE_NUMBER_1 || consoleNumber1 == RIDDLE_NUMBER_2) ||
+        (consoleNumber2 == RIDDLE_NUMBER_1 || consoleNumber2 == RIDDLE_NUMBER_2)
+    )
         println("Вы выиграли утешительный приз!")
     else
         println("Неудача!")
