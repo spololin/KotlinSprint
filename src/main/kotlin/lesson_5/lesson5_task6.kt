0e20f927da22c17873441a02e911a0688a8cb204
Sergey Pololin <spololin@gmail.com>
2025-01-22 23:10:52 +0300

    ks-5-6 removed unnecessary check

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -13,9 +13,9 @@ fun main() {
 
     print(String.format("ИМТ: %.2f. ", bodyMassIndex))
     when {
-        (bodyMassIndex < 18.5 ) -> println("Недостаточная масса тела")
-        (bodyMassIndex >= 18.5 && bodyMassIndex < 25 ) -> println("Нормальная масса тела")
-        (bodyMassIndex >= 25 && bodyMassIndex < 30 ) -> println("Избыточная масса тела")
+        bodyMassIndex < 18.5 -> println("Недостаточная масса тела")
+        bodyMassIndex >= 18.5 && bodyMassIndex < 25 -> println("Нормальная масса тела")
+        bodyMassIndex.toInt() in 25 until 30 -> println("Избыточная масса тела")
         else -> println("Ожирение")
     }
 }
