776b4cf742bf8998082f598919669b277a2cf1aa
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-24 08:21:52 +0300

    ks-5-6 removed unnecessary check

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -15,7 +15,7 @@ fun main() {
     when {
         bodyMassIndex < 18.5 -> println("Недостаточная масса тела")
         bodyMassIndex < 25 -> println("Нормальная масса тела")
-        bodyMassIndex.toInt() in 25 until 30 -> println("Избыточная масса тела")
+        bodyMassIndex < 30 -> println("Избыточная масса тела")
         else -> println("Ожирение")
     }
 }
